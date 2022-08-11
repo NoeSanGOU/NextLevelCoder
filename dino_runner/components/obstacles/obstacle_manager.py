d8791bf185a7539bc77ad8d23e7682fff57033ae
NoeSanGOU <santosnoeliayu32t32@gmail.com>
2022-08-11 14:23:13 -0400

    CLASE 3

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,33 +1,32 @@
 import pygame
+import random
+
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+#from dino_runner.utils.constants import SMALL_CACTUS
 #TAREA:
-from dino_runner.utils.constants import LARGE_CACTUS
+#from dino_runner.utils.constants import LARGE_CACTUS
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
+
     def update(self,game):
-    #si la longitud es cero , deberia añadir un obstaculo
-        #rount = 0
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS)) or self.obstacles.append(Cactus(LARGE_CACTUS))
-
-        
-            #zip() chain()
-            rount +=1
-            #aniadir otros obstaculos(cactus largos )
-            #self.obstacles.append(Cactus(SMALL_CACTUS))
-
+            cactus_type = "SMALL" if random.randint(0,1) == 0 else "LARGE"
+            self.obstacles.append(Cactus(cactus_type)) 
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
 
     #Aqui deberia dbujar el obstaculo:
     def draw(self,screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_osbstacles(self):
+        self.obstacles = []
